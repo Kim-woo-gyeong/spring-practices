f493b5d4a7f5682f781c666cc5c02e8f6f738da9
smooooch <51706587+smooooch@users.noreply.github.com>
2020-02-20 14:53:59 +0900

    usercontroller update

---
+++ hellospring/src/main/java/com/douzone/hellospring/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.douzone.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class UserController {
+	
+	@ResponseBody
+	@RequestMapping("/usr/joinform")
+	public String joinform() {
+		return "UserController:joinform";
+	}
+}
