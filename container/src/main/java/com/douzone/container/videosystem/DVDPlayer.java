816c0d81c44cbffcddc43b45f682db5608ced510
Kim-woo-gyeong <lovelykwg@naver.com>
2020-03-24 12:03:38 +0900

    명시적 설정 - xml config

--- container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
+++ container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
@@ -4,10 +4,11 @@ public class DVDPlayer {
 	private DigitalVideoDisc dvd;
 	
 	public DVDPlayer() {
-		
+		System.out.println("DVDPlayer DVDPlayer()");
 	}
 	
 	public DVDPlayer(DigitalVideoDisc dvd) {
+		System.out.println("DVDPlayer DVDPlayer(DigitalVideoDisc dvd)");
 		this.dvd = dvd;
 	}
 	
@@ -16,6 +17,7 @@ public class DVDPlayer {
 	}
 
 	public void play() {
+		System.out.println("DVDPlayer Play");
 		dvd.play();
 	}
 }
